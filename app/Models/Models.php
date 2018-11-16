63e852b8805b8a18331a5d0d0b494c8b9a3df327
tommy <tommywhy1989@gmail.com>
2018-11-16 16:41:24 +1300

    modify models

---
+++ app/Models/Models.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tommy
+ * Date: 2018/11/16
+ * Time: 下午4:38
+ */
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Models extends Model{
+
+}
